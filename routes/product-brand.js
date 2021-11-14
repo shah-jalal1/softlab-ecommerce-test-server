0ae8cf99bc6ce39d3c43e23025db762ce3b40866
shah-jalal1 <shah.jalal0894@gmail.com>
2021-11-14 15:06:02 +0600

    get all brands

--- routes/product-brand.js
+++ routes/product-brand.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // router.post('/add-brand',checkIpWhitelist,checkAdminAuth, controller.addBrand);
 router.post('/add-brand', controller.addBrand);
 // router.post('/add-multiple-brand',checkIpWhitelist,checkAdminAuth, controller.insertManyBrand);
-// router.get('/get-all-brands', controller.getAllBrands);
+router.get('/get-all-brands', controller.getAllBrands);
 // router.get('/get-brand-by-brand-id/:brandId', controller.getBrandByBrandId);
 // router.put('/edit-brand-by-brand',checkIpWhitelist,checkAdminAuth, controller.editBrandData);
 // router.post('/get-brands-by-search', controller.getParentCategoriesBySearch);
